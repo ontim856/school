e1ac942a5123ae4635156ab1df35df29dd4cbfb7
ontim856 <ontim200166@gmail.com>
2024-09-18 23:56:03 +0600

    update

--- config.php
+++ config.php
@@ -3,15 +3,7 @@ include_once('local.config.php');
 include_once('src/lib.php');
 
      
-//      // SQL query to select all data from the slides table
-     $sql = "SELECT * FROM slides";
-     
-//      // Prepare and execute the query
-     $stmt = $pdo->prepare($sql);
-     $stmt->execute();
-     
-//      // Fetch all results
-     $slides = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
      
      $docroot = $_SERVER['DOCUMENT_ROOT']."/school";
      $webroot = "http://school.akash/";
