b6e9615527aa732854dd7cee5c1d97ab09bc4324
ontim856 <ontim200166@gmail.com>
2024-10-20 07:20:05 +0600

    change

---
+++ src/BAF/slide.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\BAF\slide;
+
+
+class slide{
+   public $id;
+   public $path;
+   public $description;
+   
+   public $slides = []; // collection of slide
+
+   public function getOneSlide ($id){
+
+   }
+
+   
+   public function getFullSlides ($id){
+
+   }
+
+}
